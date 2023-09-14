361b829b07beec029556a615c26127065133c651
Wicarpessoa <wicarpp@gmail.com>
2023-09-14 20:04:13 -0300

    first commit, but it finalized

---
+++ source/server.ts
@@ -0,0 +1,25 @@
+import {fastify} from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import {  uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+
+const PORT = 3333
+const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
+
+app.listen({
+  port: PORT
+}).then(() => {
+  console.log(`HTTP Server Running on ${PORT}`)
+}
+)
